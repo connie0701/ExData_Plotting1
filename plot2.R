e44648327ff64c1725e923ed70cbc1673d5c76d2
connie0701 <connie0701@hotmail.com>
2017-01-09 22:43:24 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,3 +5,4 @@ datetime <- strptime(paste(subsetPower$Date, subsetPower$Time, sep=" "), "%d/%m/
 globalActivePower <- as.numeric(subsetPower$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
