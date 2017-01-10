86a2c458a6c31f60a7c8c4916965f91a77d5ef34
connie0701 <connie0701@hotmail.com>
2017-01-09 21:47:11 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,3 +3,4 @@ subsetPower <- power[power$Date %in% c("1/2/2007","2/2/2007") ,]
 globalActivePower<-as.numeric(subsetPower$Global_active_power)
 png("plot1.png",width = 480,height = 480)
 hist(globalActivePower,col="red",main = "Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.off()
